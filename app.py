3601ca95bfd01e7dd969fb29b58789f0e9b1506c
ravitejamillaga-gif <ravitejamillaga@gmail.com>
2026-05-28 20:41:20 +0530

    fix render deployment

--- app.py
+++ app.py
@@ -219,3 +219,9 @@ def update(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)    
