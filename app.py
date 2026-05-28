a36463732e19439a45643b218b9cfc24a495ecfa
ravitejamillaga-gif <ravitejamillaga@gmail.com>
2026-05-28 20:50:47 +0530

    fix render port issue

--- app.py
+++ app.py
@@ -217,11 +217,8 @@ def update(id):
 
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)    
+    app.run(host="0.0.0.0", port=port)
